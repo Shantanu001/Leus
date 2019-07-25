55bda0b943b2ff818625e8610186fc3410831711
Shantanu Jaiswal <shantanujaiswal.mantralabsglobal.com>
2019-07-25 17:36:26 +0530

    Updates

---
+++ precache-manifest.b465001bc9c97ac6b3294bb254f97e69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a01e2597a28bf330936",
+    "url": "/leus/static/js/runtime~main.08dc5f10.js"
+  },
+  {
+    "revision": "5b0209905cdb029adaa2",
+    "url": "/leus/static/js/main.f8ee2772.chunk.js"
+  },
+  {
+    "revision": "a5185d54e59ea0ab8b4f",
+    "url": "/leus/static/js/2.af6c4e13.chunk.js"
+  },
+  {
+    "revision": "5b0209905cdb029adaa2",
+    "url": "/leus/static/css/main.2e195483.chunk.css"
+  },
+  {
+    "revision": "a5185d54e59ea0ab8b4f",
+    "url": "/leus/static/css/2.8dde9665.chunk.css"
+  },
+  {
+    "revision": "60c39909661f3a245aae23146cd11b87",
+    "url": "/leus/index.html"
+  }
+];
